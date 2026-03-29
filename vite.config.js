5ad6249cdfb434700b130649a695b9e0a57d5482
raselmondol2344 <raselmondream245@gmail.com>
2026-03-30 01:20:27 +0600

    Activity section complete

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base:"/Digital-Tools-Buying-Website/",
   plugins: [react() , tailwindcss()],
 })
