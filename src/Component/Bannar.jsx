33e47e16203f58431ecffa015d3a21e4593ac9b5
raselmondol2344 <raselmondream245@gmail.com>
2026-03-31 09:14:02 +0600

    code updat

--- src/Component/Bannar.jsx
+++ src/Component/Bannar.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import { CiPlay1 } from "react-icons/ci";
+import LiveImg from "../../src/assets/live.png"
+import BannarImg from "../../src/assets/banner.png"
 
 const Bannar = () => {
     return (
         <div className="hero bg-base-200 min-h-screen  ">
   <div className="hero-content flex-col lg:flex-row-reverse">
-    <img
-      src="./public/banner.png"
-      className="max-w-sm rounded-lg shadow-2xl"
-    />
+    <img src ={BannarImg} className="max-w-sm rounded-lg shadow-2xl"/>
     <div className='space-y-3' >
         <div className='flex items-center gap-2 bg-blue-200 text-indigo-800 font-semibold p-2 rounded-full w-90'>
-            <img className='h-5 w-5' src="./public/live.png" alt="" />
+            <img src={LiveImg} className='h-5 w-5'  alt="" />
             <p>New: AI-Powered Tools Available</p>
         </div>
 
